6690f1c3c6d30d89689b6cb9c910894433519179
Sebastian Wolf <sebastian.wolf298@gmail.com>
2023-01-26 14:59:38 +0100

    minor tests

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,23 @@
 import { catsData } from "./data.js";
 
-console.table(catsData)
+console.log(catsData)
+
+
+
+
+
+
+
+/*
+
+<div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input
+            type="radio"
+            id="${emotion}"
+            value="${emotion}"
+            name="emotions"
+            >
+        </div>`
+
+*/
